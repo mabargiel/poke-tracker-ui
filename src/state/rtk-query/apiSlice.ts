bf2ed7aa9646704be9dfe2023faa1196adbcd377
mbargiel <mbargiel@btopservice.net>
2025-06-27 16:18:23 +0200

    Configure redux and rtk-query template

---
+++ src/state/rtk-query/apiSlice.ts
@@ -0,0 +1,12 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query";
+import type {Pokemon} from "./models/Pokemon.ts";
+
+export const pokemonApi = createApi({
+    reducerPath: 'pokemonApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
+    endpoints: (builder) => ({
+        getPokemonByName: builder.query<Pokemon, string>({
+            query: (name) => `pokemon/${name}`,
+        }),
+    }),
+})
