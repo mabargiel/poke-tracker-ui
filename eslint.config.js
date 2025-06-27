4769d181e51b03cfc2db66c0a6fad1012d63616d
mbargiel <mbargiel@btopservice.net>
2025-06-27 16:56:24 +0200

    Configure eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -4,20 +4,72 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import react from 'eslint-plugin-react'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import prettier from 'eslint-plugin-prettier'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    ...js.configs.recommended[0],
+    ...tseslint.configs.recommended[0],
+    ...react.configs.recommended[0],
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
+      prettier,
+      'jsx-a11y': jsxA11y,
+    },
+    ignores: ['dist', 'node_modules'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react/jsx-indent': ['error', 2],
+      'max-len': ['error', 120, 2],
+      'no-console': 'warn',
+      semi: ['error', 'never'],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'comma-dangle': [
+        'error',
+        {
+          arrays: 'always-multiline',
+          objects: 'always-multiline',
+          imports: 'always-multiline',
+          exports: 'always-multiline',
+          functions: 'only-multiline',
+        },
+      ],
+      'react/prop-types': ['error', { ignore: ['history'] }],
+      'jsx-a11y/label-has-associated-control': 'off',
+      'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+      'react/no-set-state': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'prefer-promise-reject-errors': 'off',
+      camelcase: 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'prettier/prettier': ['error'],
     },
   },
 ])
--- src/state/rtk-query/apiSlice.ts
+++ src/state/rtk-query/apiSlice.ts
@@ -1,12 +1,13 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query";
-import type {Pokemon} from "./models/Pokemon.ts";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+
+import type { Pokemon } from './models/Pokemon.ts'
 
 export const pokemonApi = createApi({
-    reducerPath: 'pokemonApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
-    endpoints: (builder) => ({
-        getPokemonByName: builder.query<Pokemon, string>({
-            query: (name) => `pokemon/${name}`,
-        }),
+  reducerPath: 'pokemonApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
+  endpoints: (builder) => ({
+    getPokemonByName: builder.query<Pokemon, string>({
+      query: (name) => `pokemon/${name}`,
     }),
+  }),
 })
